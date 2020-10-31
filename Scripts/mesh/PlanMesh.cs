9eddc21749dc077e402bcefbe8bc0e038f190878
Dead-Rabbit <1054222912@qq.com>
2020-10-31 09:37:26 +0800

    Update

--- Scripts/mesh/PlanMesh.cs
+++ Scripts/mesh/PlanMesh.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Resources;
 using UnityEngine;
 
 namespace mesh
@@ -6,26 +7,41 @@ namespace mesh
     [RequireComponent(typeof(MeshFilter))]
     public class PlanMesh : MonoBehaviour
     {
-        private readonly Vector3[] _vertices = new Vector3[4];
-        private readonly int[] _triangles = new int[6];
-
         #region Mesh
 
         private MeshFilter _meshFilter;
         private Mesh _mesh;
         private Material _material;
+        
+        private readonly Vector3[] _vertices = new Vector3[4];
+        private readonly int[] triangles = new int[6];
+        
+        #endregion
+
+        #region 控制球
+
+        public Boolean OpenBallControl = true;
+        public GameObject controlBallPrefab;
+        private GameObject[] _controlBalls;
 
         #endregion
-        
+
         private void Awake()
         {
-            gameObject.AddComponent<MeshRenderer>();
-            _meshFilter = gameObject.AddComponent<MeshFilter>();
-            _meshFilter.mesh = _mesh = new Mesh();
+            OpenBallControl = controlBallPrefab != null;
+            GetComponent<MeshFilter>().mesh = _mesh = new Mesh();
             _mesh.name = "自定义平面";
             
+            // 创建平面
             CreateMinePlan();
         }
+
+        private void Update()
+        {
+            
+        }
+
+        #region Mesh
 
         /// <summary>
         /// 创建自定义平面
@@ -38,20 +54,33 @@ namespace mesh
             _vertices[3] = new Vector3(2, 1);
             _mesh.vertices = _vertices;
 
-            _triangles[0] = 0;
-            _triangles[1] = 1;
-            _triangles[2] = 2;
-            _triangles[3] = 2;
-            _triangles[4] = 1;
-            _triangles[5] = 3;
-            _mesh.triangles = _triangles;
+            triangles[0] = 0;
+            triangles[1] = 1;
+            triangles[2] = 2;
+            triangles[3] = 2;
+            triangles[4] = 1;
+            triangles[5] = 3;
+            _mesh.triangles = triangles;
+
+            CreateControlBalls();
         }
+
+        #endregion
+
+        #region 控制球
         
-        private void OnDrawGizmos () {
-            Gizmos.color = Color.black;
-            for (int i = 0; i < _vertices.Length; i++) {
-                Gizmos.DrawSphere(_vertices[i], 0.1f);
+        private void CreateControlBalls()
+        {
+            if (!controlBallPrefab)
+                return;
+            
+            _controlBalls = new GameObject[_vertices.Length];
+            for (Int32 i = 0; i < _controlBalls.Length; i++) {
+                _controlBalls[i] = Instantiate(controlBallPrefab, transform);
+                _controlBalls[i].transform.localPosition = _vertices[i];
             }
         }
+
+        #endregion
     }
 }

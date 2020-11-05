3266a7e463ea39bd12d7afb0d84ad3fb41c94901
Dead-Rabbit <1054222912@qq.com>
2020-11-05 14:43:16 +0800

    update

--- LowPolygon/ZXPolygonGame/Scripts/zxVehicle/plane/ZXPlaneWingManager.cs
+++ LowPolygon/ZXPolygonGame/Scripts/zxVehicle/plane/ZXPlaneWingManager.cs
@@ -6,7 +6,12 @@ namespace zxVehicle.plane
 {
     public class ZXPlaneWingManager : MonoBehaviour
     {
-        private ZXPlane plane;
+        #region 飞机相关
+
+        private ZXPlane _plane;
+        private Rigidbody _planeRigidbody;
+
+        #endregion
         
         private struct WingPlateInfo
         {
@@ -19,48 +24,49 @@ namespace zxVehicle.plane
             }
         }
 
-        [Range(0, 10)]
-        public float simulateSpeed = 0;
+        [Range(0, 10000)] public float controlPlatePower = 0;
+        [Range(0, 10000)] public float floatPlatePower = 0;
         
         // 旋转板
-        public GameObject[] leftPlates;
-        public GameObject[] rightPlates;
+        public GameObject[] leftControlPlates;
+        public GameObject[] rightControlPlates;
         public GameObject[] leftPlateAxles;
         public GameObject[] rightPlateAxles;
-        private List<WingPlateInfo> _leftWingPlateInfos = new List<WingPlateInfo>();
-        private List<WingPlateInfo> _rightWingPlateInfos = new List<WingPlateInfo>();
+        public GameObject[] planePanelWing;
+        private List<WingPlateInfo> _leftControlWingPlateInfos = new List<WingPlateInfo>();
+        private List<WingPlateInfo> _righControltWingPlateInfos = new List<WingPlateInfo>();
 
         private float _preInputHorizontal = 0;
 
         void Awake()
         {
-            // 找到对应飞机
-            plane = transform.parent.GetComponent<ZXPlane>();
-            if (null == plane) {
+            _plane = transform.parent.GetComponent<ZXPlane>();
+            if (null == _plane) {
                 Debug.LogError("机翼未找到宿主飞机");
                 return;
             }
+            _planeRigidbody = _plane.GetComponent<Rigidbody>();
             
-            for (Int32 i = 0; i < leftPlates.Length; i++) {
-                GameObject plate = leftPlates[i];
+            for (Int32 i = 0; i < leftControlPlates.Length; i++) {
+                GameObject plate = leftControlPlates[i];
                 if (null == plate) continue;
                 
                 if (i >= leftPlateAxles.Length) continue;
                 GameObject axle = leftPlateAxles[i];
                 if (null == axle) continue;
                 
-                _leftWingPlateInfos.Add(new WingPlateInfo(plate.transform, axle.transform.right));
+                _leftControlWingPlateInfos.Add(new WingPlateInfo(plate.transform, axle.transform.right));
             }
             
-            for (Int32 i = 0; i < rightPlates.Length; i++) {
-                GameObject plate = rightPlates[i];
+            for (Int32 i = 0; i < rightControlPlates.Length; i++) {
+                GameObject plate = rightControlPlates[i];
                 if (null == plate) continue;
                 
                 if (i >= rightPlateAxles.Length) continue;
                 GameObject axle = rightPlateAxles[i];
                 if (null == axle) continue;
                 
-                _rightWingPlateInfos.Add(new WingPlateInfo(plate.transform, axle.transform.right));
+                _righControltWingPlateInfos.Add(new WingPlateInfo(plate.transform, axle.transform.right));
             }
         }
 
@@ -71,10 +77,10 @@ namespace zxVehicle.plane
             // 测试
             float diffOfHorizontal = (inputHorizontal - _preInputHorizontal) * 10;
             _preInputHorizontal = inputHorizontal;
-            foreach (WingPlateInfo plateInfo in _leftWingPlateInfos) {
+            foreach (WingPlateInfo plateInfo in _leftControlWingPlateInfos) {
                 plateInfo.plate.Rotate(-plateInfo.plateAxle, diffOfHorizontal);
             }
-            foreach (WingPlateInfo plateInfo in _rightWingPlateInfos) {
+            foreach (WingPlateInfo plateInfo in _righControltWingPlateInfos) {
                 plateInfo.plate.Rotate(plateInfo.plateAxle, diffOfHorizontal);
             }
             
@@ -83,10 +89,22 @@ namespace zxVehicle.plane
             #region 飞行效果
 
             // 模拟速度
-            Vector3 velocity = plane.transform.forward * simulateSpeed;
+            // TODO 设定控制方向 - z方向减弱控制，加强y方向的控制
+            // TODO 受力根据速度大小来确定力度
+            Vector3 controlForce = _planeRigidbody.velocity.normalized * controlPlatePower;
             // 计算飞行给面板带来的力
-            foreach (WingPlateInfo plateInfo in _leftWingPlateInfos) {
-                // plateInfo.plate.up
+            foreach (WingPlateInfo plateInfo in _leftControlWingPlateInfos) {
+                Transform plate = plateInfo.plate;
+                _planeRigidbody.AddForceAtPosition(controlForce, plate.position);
+            }
+            foreach (WingPlateInfo plateInfo in _righControltWingPlateInfos) {
+                Transform plate = plateInfo.plate;
+                _planeRigidbody.AddForceAtPosition(controlForce, plate.position);
+            }
+
+            Vector3 floatForce = _planeRigidbody.velocity.normalized * floatPlatePower;
+            foreach (GameObject plate in planePanelWing) {
+                _planeRigidbody.AddForceAtPosition(floatForce, plate.transform.position);
             }
 
             #endregion
